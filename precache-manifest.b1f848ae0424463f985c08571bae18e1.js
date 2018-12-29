a8ba6e195c77b80f0474ba41760bfe257a1d8241
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-12-29 08:17:36 +0530

    Updates

---
+++ precache-manifest.b1f848ae0424463f985c08571bae18e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e67cf6ea91624fea5a5f",
+    "url": "/Pixabay-Image-Finder/static/js/runtime~main.e67cf6ea.js"
+  },
+  {
+    "revision": "c920d97bff97bbafef18",
+    "url": "/Pixabay-Image-Finder/static/js/main.c920d97b.chunk.js"
+  },
+  {
+    "revision": "7627b80504d037724aab",
+    "url": "/Pixabay-Image-Finder/static/js/1.7627b805.chunk.js"
+  },
+  {
+    "revision": "c920d97bff97bbafef18",
+    "url": "/Pixabay-Image-Finder/static/css/main.ec3d9e9e.chunk.css"
+  },
+  {
+    "revision": "31936f8334879b6cda189655d9af0244",
+    "url": "/Pixabay-Image-Finder/index.html"
+  }
+];
